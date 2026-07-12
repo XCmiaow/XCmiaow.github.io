5643faa9385c80b9bb992e48e7f746c87a540b9b
Fang Xujie <2332797170@qq.com>
2026-07-12 04:17:20 -0700

    build: add mdx and astro type checks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://XCmiaow.github.io',
   base: '/',
+  integrations: [mdx()],
   i18n: {
     defaultLocale: 'zh',
     locales: ['zh', 'en'],
